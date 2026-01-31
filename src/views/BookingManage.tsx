d9eb45db4dd6896eba1fb7a62d1700d916a780fd
518luck <165388484+518luck@users.noreply.github.com>
2026-01-31 23:15:28 +0800

    feat(预定管理): 添加预定管理页面表格展示功能
    - 引入 dayjs 依赖用于日期时间格式化
    - 实现预定管理页面表格组件，展示会议室预定信息
    - 表格包含会议室名称、位置、预定人、时间、状态等列
    - 添加分页功能支持

--- src/views/BookingManage.tsx
+++ src/views/BookingManage.tsx
@@ -1,8 +1,118 @@
+import {
+  Badge,
+  Button,
+  DatePicker,
+  Form,
+  Input,
+  Popconfirm,
+  Table,
+  TimePicker,
+  message,
+  type TableProps,
+} from "antd";
+import { useEffect, useState } from "react";
+import type { UserItem } from "@/api/login";
+import type { MeetingRoomItem } from "@/types/meeting-room.type";
+import dayjs from "dayjs";
+
+interface BookingSearchResult {
+  id: number;
+  startTime: string;
+  endTime: string;
+  status: string;
+  note: string;
+  createTime: string;
+  updateTime: string;
+  user: UserItem;
+  room: MeetingRoomItem;
+}
+
 const BookingManage = () => {
+  const [pageNo, setPageNo] = useState<number>(1);
+  const [pageSize, setPageSize] = useState<number>(10);
+  const [bookingSearchResult] = useState<Array<BookingSearchResult>>([]);
+
+  const columns: TableProps<BookingSearchResult>["columns"] = [
+    {
+      title: "会议室名称",
+      dataIndex: "room",
+      render(_, record) {
+        return record.room.name;
+      },
+    },
+    {
+      title: "会议室位置",
+      dataIndex: "room",
+      render(_, record) {
+        return record.room.location;
+      },
+    },
+    {
+      title: "预定人",
+      dataIndex: "user",
+      render(_, record) {
+        return record.user.username;
+      },
+    },
+    {
+      title: "开始时间",
+      dataIndex: "startTime",
+      render(_, record) {
+        return dayjs(new Date(record.startTime)).format("YYYY-MM-DD HH:mm:ss");
+      },
+    },
+    {
+      title: "结束时间",
+      dataIndex: "endTime",
+      render(_, record) {
+        return dayjs(new Date(record.endTime)).format("YYYY-MM-DD HH:mm:ss");
+      },
+    },
+    {
+      title: "审批状态",
+      dataIndex: "status",
+    },
+    {
+      title: "预定时间",
+      dataIndex: "createTime",
+      render(_, record) {
+        return dayjs(new Date(record.createTime)).format("YYYY-MM-DD hh:mm:ss");
+      },
+    },
+    {
+      title: "备注",
+      dataIndex: "note",
+    },
+    {
+      title: "描述",
+      dataIndex: "description",
+    },
+    {
+      title: "操作",
+      render: (_, record) => <div></div>,
+    },
+  ];
+
+  const changePage = function (pageNo: number, pageSize: number) {
+    setPageNo(pageNo);
+    setPageSize(pageSize);
+  };
+
   return (
-    <div>
-      <h1>预定管理</h1>
+    <div id="bookingManage-container">
+      <div className="bookingManage-table">
+        <Table
+          columns={columns}
+          dataSource={bookingSearchResult}
+          pagination={{
+            current: pageNo,
+            pageSize: pageSize,
+            onChange: changePage,
+          }}
+        />
+      </div>
     </div>
   );
 };
+
 export default BookingManage;
